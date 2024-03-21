146666d11b43625f4759f1d0b668845f78bf5eee
snowlxy1202 <snowlxy1202@gmail.com>
2024-03-20 19:44:01 -0700

    implement idb put and get method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  const dataStore = transaction.objectStore('jate');
+  const request = dataStore.put({ id: 1, value: content });
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readonly');
+  const dataStore = transaction.objectStore('jate');
+  const request = dataStore.get(1);
+  const result = await request;
+  return result?.value;
+}
 
 initdb();
